7c026d05c1a4fffad708a0ae83cb9ed4643ca306
Julia Crosara <47763769+juliacrosara@users.noreply.github.com>
2019-05-17 21:08:24 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,18 +1,35 @@
 /*
  * Create a list that holds all of your cards
  */
+const deck = document.querySelector('.deck');
+let toggledCards = [];
+let moves = 0;
 
+function shuffleDeck() {
+    const cardsToShuffle = Array.from(document.querySelectorAll('.card'));
+    const shuffledCards = shuffle(cardsToShuffle);
+    for (card of shuffledCards) {
+        deck.appendChild(card);
+    }
+}
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+shuffleDeck();
+
+deck.addEventListener('click', function() {
+    const clickTarget = event.target;
+    if (isClickValid(clickTarget)) {
+        toggleCard(clickTarget);
+        addToggleCard(clickTarget);
+        if (toggledCards.length === 2) {
+            checkForMatch(clickTarget);
+        }
+    }
+});
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -24,6 +41,47 @@ function shuffle(array) {
 
     return array;
 }
+
+
+
+
+function isClickValid(clickTarget) {
+    return (
+        clickTarget.classList.contains('card') &&
+        !clickTarget.classList.contains('match') &&
+        toggledCards.length < 2 &&
+        !toggledCards.includes(clickTarget)
+    );
+}
+
+
+function toggleCard(card) {
+    card.classList.toggle('open');
+    card.classList.toggle('show');
+}
+
+function addToggleCard(clickTarget) {
+    toggledCards.push(clickTarget);
+    console.log(toggledCards);
+}
+
+function checkForMatch() {
+    if (
+        toggledCards[0].firstElementChild.className === toggledCards[1].firstElementChild.className
+    ) {
+        toggledCards[0].classList.toggle('match');
+        toggledCards[1].classList.toggle('match');
+        toggledCards = [];
+    } else {
+        setTimeout(function() {
+            toggleCard(toggledCards[0]);
+            toggleCard(toggledCards[1]);
+            toggledCards = [];
+        }, 2000);
+    }
+}
+
+
 
 
 /*
